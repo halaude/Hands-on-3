9519d1b95edc0de82d15af8fd6d088fe92624845
halaude <131706015+halaude@users.noreply.github.com>
2024-09-25 14:06:35 -0600

    Create helloworld.sh

---
+++ helloworld/helloworld.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Imprimir "Hello World"
+echo "Hello World"
+
+# Listar el contenido del directorio actual
+echo "Listando el contenido del directorio actual:"
+ls
+
+# Crear un directorio llamado Test
+echo "Creando el directorio Test"
+mkdir Test
+
+# Cambiarse al directorio Test
+cd Test
+
+# Listar el contenido del directorio Test
+echo "Listando el contenido del directorio Test:"
+ls
+
+#ponemos permisos no lo olvides con chmod +x helloworld.sh
